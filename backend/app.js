8be53abd191d7111943321489b6cc26ed12a551c
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-24 17:33:24 +0500

    MERN_AUTH

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,12 @@
 // import express from 'express'
 const mongoose = require('mongoose')
 const express = require('express')
+// const dotenv = require('dotenv')
+require('dotenv').config()
 
 const app = express()
 
-mongoose.connect('mongodb+srv://mern_auth:mern_auth@cluster0.gn7dyr9.mongodb.net/mern_auth?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://mern_auth:${process.env.MONGO_PASSWORD}@cluster0.gn7dyr9.mongodb.net/mern_auth?retryWrites=true&w=majority`)
     .then(() => {
         app.listen(5000)
         console.log('Database is connected');
