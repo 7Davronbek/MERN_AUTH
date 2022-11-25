414b542e8c4b6f88998d3a2036a8564245ae0afa
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-25 15:33:54 +0500

    MERN_AUTH

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -56,11 +56,12 @@ const login = async (req, res, next) => {
     }
 
     const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password)
+
     if (!isPasswordCorrect) {
         return res.status(400).json({ message: 'Invalid Email / Password' })
     }
 
-    const token = jwt.sign({ id: existingUser._id, JWT_SECRET_KEY }, {
+    const token = jwt.sign({ id: existingUser._id }, JWT_SECRET_KEY, {
         expiresIn: '1hr'
     })
 
@@ -68,5 +69,42 @@ const login = async (req, res, next) => {
 
 }
 
+const verifyToken = (req, res, next) => {
+    const headers = req.headers[`authorization`]
+    const token = headers.split(' ')[1]
+
+    if (!token) {
+        return res.status(404).json({ message: 'Token not found' })
+    }
+
+    jwt.verify(String(token), JWT_SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(400).json({ message: 'Invalid token' })
+        }
+        req.id = user.id
+    })
+    next()
+}
+
+const getUser = async (req, res, next) => {
+
+    const userId = req.id
+    let user
+
+    try {
+        user = await User.findById(userId, '-password')
+    } catch (err) {
+        return new Error(err)
+    }
+
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' })
+    }
+
+    return res.status(200).json({ user })
+}
+
 exports.signup = signup
 exports.login = login
+exports.verifyToken = verifyToken
+exports.getUser = getUser
