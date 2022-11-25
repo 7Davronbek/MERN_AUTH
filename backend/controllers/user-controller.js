3209bc71dc4374963258d6f400270bfb6c04ee4e
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-25 11:35:40 +0500

    MERN_AUTH

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,5 +1,7 @@
 const User = require('../model/User')
+const bcrypt = require('bcryptjs')
 
+// SIGN UP
 const signup = async (req, res, next) => {
 
     const { name, email, password } = req.body
@@ -16,10 +18,12 @@ const signup = async (req, res, next) => {
         return res.status(400).json({ message: 'User already exist' })
     }
 
+    const hashedPassword = bcrypt.hashSync(password)
+
     const user = new User({
         name,
         email,
-        password
+        password: hashedPassword
     })
 
     try {
@@ -33,4 +37,30 @@ const signup = async (req, res, next) => {
     return res.status(201).json({ message: user })
 }
 
+// LOGIN
+const login = async (req, res, next) => {
+    const { email, password } = req.body
+
+    let existingUser
+
+    try {
+        existingUser = await User.findOne({ email: email })
+    } catch (err) {
+        console.log(err);
+    }
+
+    if (!existingUser) {
+        return res.status(400).json({ message: 'User not found, Singup please' })
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password)
+    if (!isPasswordCorrect) {
+        return res.status(400).json({ message: 'Invalid Email / Password' })
+    }
+
+    return res.status(200).json({ message: 'Successfully Logged in' })
+
+}
+
 exports.signup = signup
+exports.login = login
