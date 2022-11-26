8ec26b5e25d1189c0b3028a64ed2e5534aae6e22
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-26 16:48:30 +0500

    MERN_AUTH

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,10 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}))
 app.use(cookieParser())
 app.use(express.json())
 app.use('/api', router)
